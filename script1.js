dc72db5a1fcc6cb90d96bf9ec6de68b45e4ae8d4
vasya-bo <102417441+vasya-bo@users.noreply.github.com>
2022-04-13 14:15:14 +0300

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-function positiveSum(arr) {
-  let  arrPositive =  arr.filter(c=> c>=0)
-   if (arrPositive.length > 0) {
-   return arrPositive.reduce((a,b) => a+b)
- } else { return 0}
- };
-
- module.exports = {positiveSum};
